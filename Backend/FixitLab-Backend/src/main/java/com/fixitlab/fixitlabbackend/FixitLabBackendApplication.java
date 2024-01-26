f1fd99eff9a4ea77af6de8b38ab3fa958189e37d
chamal995 <chamal995@gmail.com>
2024-01-26 17:33:01 +0530

    Initialize spring boot project for backend

---
+++ Backend/FixitLab-Backend/src/main/java/com/fixitlab/fixitlabbackend/FixitLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fixitlab.fixitlabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixitLabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixitLabBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/FixitLab-Backend/src/test/java/com/fixitlab/fixitlabbackend/FixitLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixitlab.fixitlabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitLabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
